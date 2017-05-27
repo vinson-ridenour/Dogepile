a81c1026167d8e139cae64035c589ef675adf8d9
mfmunic <mfmunic@gmail.com>
2017-05-27 16:04:28 -0700

    Started framework
    added html, still need to make table for results.
    added css to position elements on page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+//------------transition from search view to main view-------------------------------
+$(".init").on("click", function (event){
+	event.preventDefault();
+
+	$("#searchContainer").css("visibility", "hidden");
+	$("header").css("visibility", "visible");
+	$("#mainContainer").css("visibility", "visible");
+})
+
 });
