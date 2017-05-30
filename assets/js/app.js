02b60c18e1fb999bea9b287d7d44dd42006a78d6
mfmunic <mfmunic@gmail.com>
2017-05-30 15:55:19 -0700

    With added style and swagger
    incorp some of Vinse changes and added tranistion from search to main

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,20 @@ $(document).ready(function(){
 $(".init").on("click", function (event){
 	event.preventDefault();
 
-	$("#searchContainer").css("visibility", "hidden");
-	$("header").css("visibility", "visible");
-	$("#mainContainer").css("visibility", "visible");
+	$("#searchContainer").css("opacity", "0");
+	
+	var showMain =setTimeout (function(){
+		$("header").css("box-shadow", "0 0 5px");
+	}, 1000)
+
+	var showMain =setTimeout (function(){
+		$("#headerContainer").css("opacity", "1");
+		$("#mainContainer").css("opacity", "1");
+		$("footer").css("opacity", "1");
+		$("#searchContainer").css("position", "absolute");
+	}, 2000)
+	
+	
 })
 
 });
