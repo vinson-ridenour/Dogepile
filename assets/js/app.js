3681ac351314b8eda941c33a5ca61b80b3ce039d
vinson-ridenour <vinsonridenour@Vinses-MacBook-Pro.local>
2017-05-31 19:15:33 -0700

    resolving conflicts round 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 //------------transition from search view to main view-------------------------------
-<<<<<<< HEAD
 	$(".init").on("click", function (event){
 		event.preventDefault();
 
@@ -18,27 +17,5 @@ $(document).ready(function() {
 			$("footer").css("opacity", "1");
 			$("#searchContainer").css("position", "absolute");
 		}, 2000);
-	
 	});
-=======
-$(".init").on("click", function (event){
-	event.preventDefault();
-
-	$("#searchContainer").css("opacity", "0");
-	
-	var showMain =setTimeout (function(){
-		$("header").css("box-shadow", "0 0 5px");
-	}, 1000)
-
-	var showMain =setTimeout (function(){
-		$("#headerContainer").css("opacity", "1");
-		$("#mainContainer").css("opacity", "1");
-		$("footer").css("opacity", "1");
-		$("#searchContainer").css("position", "absolute");
-	}, 2000)
-	
-	
-})
->>>>>>> 973fa71791de9e3e81391011d68117b2ba01c008
-
 });
