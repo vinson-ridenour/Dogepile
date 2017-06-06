7757c78d481203792edbc455ecaf9c67437080ed
andyhe829 <andyhe829@gmail.com>
2017-06-05 19:26:01 -0700

    add function addUserVenueToFirebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -9,11 +9,13 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-function saveDataInFirebase(filename) {
+
+
+function moveJsonTofirebase(filename) {
   $.getJSON(filename, function(json) {
     for (var i = 0; i < json[Object.keys(json)[0]].length; i++) {
       if (!json[Object.keys(json)[0]][i].address || json[Object.keys(json)[0]][i].address == "") {
-        console.log("Empty address found for " + json[Object.keys(json)[0]][i].name + " (" + Object.keys(json)[0] + "), skip it.");
+        console.log("Empty address found in " + json[Object.keys(json)[0]][i].name + " (" + Object.keys(json)[0] + "), skip it.");
         continue;
       }
       database.ref(Object.keys(json)[0]+ "/" + i).set({
@@ -25,5 +27,25 @@ function saveDataInFirebase(filename) {
         lng:     getCoorFromAddress(json[Object.keys(json)[0]][i].address).lng
       });
     }
+  });
+}
+
+function addUserVenueToFirebase(category, userVenue) {
+  if (category != "hotels" || category != "restaurants" || category != "parks") {
+    console.log("Invalid category name.");
+    return;
+  }
+  if (!userVenue.address || userVenue.address == "") {
+    console.log("Empty address found in user input venue.");
+    continue;
+  }
+  var indexForUserVenue = database.ref(Object.keys(json)[0]).length;
+  database.ref(Object.keys(json)[0]+ "/" + i).set({
+    address: userVenue.address,
+    imgURL:  userVenue.imgURL,
+    name:    userVenue.name,
+    phone:   userVenue.phone,
+    lat:     getCoorFromAddress(userVenue.address).lat,
+    lng:     getCoorFromAddress(userVenue.address).lng
   });
 }
