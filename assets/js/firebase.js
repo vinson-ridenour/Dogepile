e154c8e6e492fd753d5bf86ab4243eb80697974e
Joseph Huynh <joe.huynh@gmail.com>
2017-06-05 21:06:21 -0700

    Worked on firebase search functions

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -13,28 +13,40 @@ var database = firebase.database();
 
 // Reads a json file and stores venues in Firebase
 function saveDataInFirebase(filename) {
-      $.getJSON(filename, function(json) {
-    for (var i = 0; i < json[Object.keys(json)[0]].length; i++) {
-      if (!json[Object.keys(json)[0]][i].address || json[Object.keys(json)[0]][i].address == "") {
-        console.log("Empty address found for " + json[Object.keys(json)[0]][i].name + " (" + Object.keys(json)[0] + "), skip it.");
-        continue;
-      }
-      database.ref(Object.keys(json)[0]+ "/" + i).set({
-        address: json[Object.keys(json)[0]][i].address,
-        imgURL:  json[Object.keys(json)[0]][i].imgURL,
-        name:    json[Object.keys(json)[0]][i].name,
-        phone:   json[Object.keys(json)[0]][i].phone,
-        lat:     getCoorFromAddress(json[Object.keys(json)[0]][i].address).lat,
-        lng:     getCoorFromAddress(json[Object.keys(json)[0]][i].address).lng
-      });
-    }
-  });
+    $.getJSON(filename, function(json) {
+        for (var i = 0; i < json[Object.keys(json)[0]].length; i++) {
+            if (!json[Object.keys(json)[0]][i].address || json[Object.keys(json)[0]][i].address == "") {
+                console.log("Empty address found for " + json[Object.keys(json)[0]][i].name + " (" + Object.keys(json)[0] + "), skip it.");
+                continue;
+            }
+            database.ref(Object.keys(json)[0] + "/" + i).set({
+                address: json[Object.keys(json)[0]][i].address,
+                imgURL: json[Object.keys(json)[0]][i].imgURL,
+                name: json[Object.keys(json)[0]][i].name,
+                phone: json[Object.keys(json)[0]][i].phone,
+                lat: getCoorFromAddress(json[Object.keys(json)[0]][i].address).lat,
+                lng: getCoorFromAddress(json[Object.keys(json)[0]][i].address).lng
+            });
+        }
+    });
 }
 
 // Returns an array of locations within the radius (for given category)
 function searchCategory(address, category, radius) {
-    if (category != "restaurants" && category != "hotels" && category != "parks") {
-        let resultArr = filterByDistance();
+    if (category == "restaurants" || category == "hotels" || category == "parks") {
+        console.log("Searching in " + category);
+        let categoryRef = database.ref(category);
+        let categoryArr = categoryRef.once("value", function(data) {
+            console.log("Inside searchCategory...");
+            console.log(data.val());
+            //let addr = getCoorFromAddress(address);
+            let resultArr = filterByDistance({ lat: 32.8604494, lng: -117.2205901 }, radius, data.val());
+            return resultArr;
+        });
+    }
+    // Query meetup for locations
+    else if (category == "meetups") {
+
     } else {
         console.log("Invalid category!");
     }
