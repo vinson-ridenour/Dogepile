6bd1edc18300c8c58c9f92bfdd724c01265a6b92
vinson-ridenour <vinsonridenour@Vinses-MacBook-Pro.local>
2017-06-09 14:52:10 -0700

    changed radiusBtn color, text color, and dropdown option colors to be aligned with rest of the site buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
         if ($(this).attr("id") === "searchBtn" && $("#icon_prefix").val().length > 0) {
 
-            $("#addressDisplay").text($("#icon_prefix").val());
+            $("#locationEntered").text($("#icon_prefix").val());
             console.log("Searching...");
             startAddr = $("#icon_prefix").val();
             searchAll(startAddr, 2);
